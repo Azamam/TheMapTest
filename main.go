426ab739cf404906a7588f3c3d1c3685d303f432
prafick <prafick3@gmail.com>
2019-08-11 13:31:25 +0300

    Refactoring

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,66 +9,60 @@ import (
 	"time"
 )
 
-const (
-	WrongCardNumber      = 1
-	WrongCardExpiredDate = 2
-	WrongCardHolder      = 3
-	WrongCVV             = 4
-	WrongOrderID         = 5
-	WrongAmount          = 6
-	CardHasExpired       = 7
-	InsufficientFunds    = 8
-	UnableToExecute      = 9
+type (
+	ErrorDesc struct {
+		ErrorCode int    `json:"error_code"`
+		ErrorMsg  string `json:"error_msg"`
+	}
+
+	Error struct {
+		Error ErrorDesc `json:"error"`
+	}
+
+	Card struct {
+		PAN           string `json:"pan"`
+		EMonth        int    `json:"e_month"`
+		EYear         int    `json:"e_year"`
+		CVV           int    `json:"cvv"`
+		Holder        string `json:"holder"`
+		amountOnCard  int
+		blockedAmount int
+	}
+
+	Deal struct {
+		OrderID string `json:"order_id"`
+		Amount  int    `json:"amount"`
+		DealID  int    `json:"deal_id"`
+	}
+
+	Transaction struct {
+		MerchantContractID int  `json:"merchant_contract_id"`
+		Card               Card `json:"card"`
+		Deal               Deal `json:"deal"`
+	}
 )
 
-var errorText = map[int]string{
-	WrongCardNumber:      "Wrong card number",
-	WrongCardExpiredDate: "Wrong card expired date",
-	WrongCardHolder:      "Wrong card holder",
-	WrongCVV:             "Wrong CVV",
-	WrongOrderID:         "Wrong order id",
-	WrongAmount:          "Wrong order Amount",
-	CardHasExpired:       "Card Has Expired",
-	InsufficientFunds:    "You do not have enough funds on the card",
-	UnableToExecute:      "Unable to execute",
+var (
+	WrongCardNumber      = myOnlyError(1, "Wrong card number")
+	WrongCardExpiredDate = myOnlyError(2, "Wrong card expired date")
+	WrongCardHolder      = myOnlyError(3, "Wrong card holder")
+	WrongCVV             = myOnlyError(4, "Wrong CVV")
+	WrongOrderID         = myOnlyError(5, "Wrong order id")
+	WrongAmount          = myOnlyError(6, "Wrong order Amount")
+	CardHasExpired       = myOnlyError(7, "Card Has Expired")
+	InsufficientFunds    = myOnlyError(8, "You do not have enough funds on the card")
+	UnableToExecute      = myOnlyError(9, "Unable to execute")
+
+	counterTransaction = 1
+	transactions       = make(map[int]Transaction)
+	cards              = make(map[string]Card)
+)
+
+func myOnlyError(code int, errorMsg string) Error {
+	return Error{ErrorDesc{code, errorMsg}}
 }
 
-type ErrorDesc struct {
-	ErrorNumber int    `json:"error_number"`
-	ErrorMsg    string `json:"error_msg"`
-}
-
-type Error struct {
-	Error ErrorDesc `json:"error"`
-}
-
-type Card struct {
-	PAN           string `json:"pan"`
-	EMonth        int    `json:"e_month"`
-	EYear         int    `json:"e_year"`
-	CVV           int    `json:"cvv"`
-	Holder        string `json:"holder"`
-	amountOnCard  int
-	blockedAmount int
-}
-
-type Deal struct {
-	OrderID string `json:"order_id"`
-	Amount  int    `json:"amount"`
-	DealID  int    `json:"deal_id"`
-}
-
-type Transaction struct {
-	MerchantContractID int  `json:"merchant_contract_id"`
-	Card               Card `json:"card"`
-	Deal               Deal `json:"deal"`
-}
-
-var counter = 1
-var transactions = make(map[int]Transaction)
-var cards = make(map[string]Card)
-
-func checkCardNumber(purportedCC string) bool {
+func isValidCardNumber(purportedCC string) bool {
 	if purportedCC != "" {
 		purportedCC = strings.ReplaceAll(purportedCC, " ", "")
 		var sum = 0
@@ -91,7 +84,7 @@ func checkCardNumber(purportedCC string) bool {
 	return false
 }
 
-func checkCardExpiryDate(eMonth int, eYear int) bool {
+func isValidCardExpiryDate(eMonth int, eYear int) bool {
 	currentDate := time.Now()
 	if currentDate.Year() > eYear {
 		return false
@@ -102,43 +95,36 @@ func checkCardExpiryDate(eMonth int, eYear int) bool {
 	}
 }
 
-func validateData(transaction Transaction) (Error, bool) {
-	if checkCardNumber(transaction.Card.PAN) {
-		if checkCardExpiryDate(transaction.Card.EMonth, transaction.Card.EYear) {
-			if transaction.Card.Holder != "" {
-				if transaction.Card.CVV > 99 && transaction.Card.CVV < 1000 {
-					if transaction.Deal.OrderID != "" {
-						if transaction.Deal.Amount > 0 {
-							return Error{}, true
-						} else {
-							return Error{ErrorDesc{WrongAmount, errorText[WrongAmount]}}, false
-						}
-					} else {
-						return Error{ErrorDesc{WrongOrderID, errorText[WrongOrderID]}}, false
-					}
-				} else {
-					return Error{ErrorDesc{WrongCVV, errorText[WrongCVV]}}, false
-				}
-			} else {
-				return Error{ErrorDesc{WrongCardHolder, errorText[WrongCardHolder]}}, false
-			}
-		} else {
-			if transaction.Card.EMonth > 0 && transaction.Card.EYear > 1970 {
-				return Error{ErrorDesc{CardHasExpired, errorText[CardHasExpired]}}, false
-			}
-			return Error{ErrorDesc{WrongCardExpiredDate, errorText[WrongCardExpiredDate]}}, false
-		}
-	} else {
-		return Error{ErrorDesc{WrongCardNumber, errorText[WrongCardNumber]}}, false
+func (transaction *Transaction) validate() (Error, bool) {
+	if !isValidCardNumber(transaction.Card.PAN) {
+		return WrongCardNumber, false
 	}
-
+	if !isValidCardExpiryDate(transaction.Card.EMonth, transaction.Card.EYear) {
+		if transaction.Card.EMonth > 0 && transaction.Card.EYear > 1970 {
+			return CardHasExpired, false
+		}
+		return WrongCardExpiredDate, false
+	}
+	if transaction.Card.Holder == "" || transaction.Card.Holder != cards[transaction.Card.PAN].Holder {
+		return WrongCardHolder, false
+	}
+	if transaction.Card.CVV < 99 && transaction.Card.CVV > 1000 {
+		return WrongCVV, false
+	}
+	if transaction.Deal.OrderID == "" {
+		return WrongOrderID, false
+	}
+	if transaction.Deal.Amount <= 0 {
+		return WrongAmount, false
+	}
+	return Error{}, true
 }
 
-func validateDeal(card Card, dealAuth Deal) (Error, bool) {
+func (dealAuth *Deal) validate(card Card) (Error, bool) {
 	if card.amountOnCard >= dealAuth.Amount {
 		return Error{}, true
 	} else {
-		return Error{ErrorDesc{InsufficientFunds, errorText[InsufficientFunds]}}, false
+		return InsufficientFunds, false
 	}
 }
 
@@ -153,35 +139,37 @@ func Block(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Fatal(w, "err %q\n", err, err.Error())
-		} else {
-			var transaction Transaction
-			err = json.Unmarshal(body, &transaction)
-			if err != nil {
-				log.Fatal(w, "Can't unmarshal: ", err.Error())
-			} else {
-				errJson, ok := validateData(transaction)
-				if ok {
-					transaction.Deal.DealID = counter
-					counter += 1
-					transactions[transaction.Deal.DealID] = transaction
-					if card, ok := cards[transaction.Card.PAN]; ok {
-						card.blockedAmount = transaction.Deal.Amount
-						card.amountOnCard -= card.blockedAmount
-						cards[card.PAN] = card
-						deal := make(map[string]int)
-						deal["deal_id"] = transaction.Deal.DealID
-						fmt.Println("Block:", cards[transaction.Card.PAN])
-						_ = json.NewEncoder(w).Encode(deal)
-					}
+		}
+		var transaction Transaction
+		err = json.Unmarshal(body, &transaction)
+		if err != nil {
+			log.Fatal(w, "Can't unmarshal: ", err.Error())
+		}
+		if errJson, ok := transaction.validate(); ok {
+			transaction.Deal.DealID = counterTransaction
+			counterTransaction += 1
+			transactions[transaction.Deal.DealID] = transaction
+			if card, ok := cards[transaction.Card.PAN]; ok {
+				if errJson, ok = transaction.Deal.validate(card); ok {
+					card.blockedAmount += transaction.Deal.Amount
+					card.amountOnCard -= transaction.Deal.Amount
+					cards[card.PAN] = card
+					deal := make(map[string]int)
+					deal["deal_id"] = transaction.Deal.DealID
+					log.Println("Block:", cards[transaction.Card.PAN])
+					_ = json.NewEncoder(w).Encode(deal)
 				} else {
+					delete(transactions, transaction.Deal.DealID)
 					_ = json.NewEncoder(w).Encode(errJson)
 				}
 			}
+		} else {
+			_ = json.NewEncoder(w).Encode(errJson)
 		}
+
 	} else {
 		w.WriteHeader(http.StatusForbidden)
 	}
-
 }
 
 func Charge(w http.ResponseWriter, r *http.Request) {
@@ -189,37 +177,27 @@ func Charge(w http.ResponseWriter, r *http.Request) {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
 			log.Fatal(w, "err %q\n", err, err.Error())
-		} else {
-			var deal Deal
-			err = json.Unmarshal(body, &deal)
-			if err != nil {
-				log.Fatal(w, "Can't unmarshal: ", err.Error())
-			} else {
-				if transact, ok := transactions[deal.DealID]; ok {
-					card := cards[transact.Card.PAN]
-					errJson, ok := validateDeal(card, transact.Deal)
-					if ok {
-						card.blockedAmount -= deal.Amount
-						cards[card.PAN] = card
-						resp := make(map[string]string)
-						resp["Operation"] = "Success"
-						delete(transactions, deal.DealID)
-						fmt.Println("Charge:", cards[transact.Card.PAN])
-						_ = json.NewEncoder(w).Encode(resp)
-					} else {
-						card.amountOnCard += transact.Card.blockedAmount
-						card.blockedAmount -= transact.Deal.Amount
-						cards[card.PAN] = card
-						_ = json.NewEncoder(w).Encode(errJson)
-					}
-				} else {
-					_ = json.NewEncoder(w).Encode(
-						Error{ErrorDesc{UnableToExecute, errorText[UnableToExecute]}})
-				}
-			}
 		}
-		w.Header().Set("Content-Type", "application/json")
+		var deal Deal
+		err = json.Unmarshal(body, &deal)
+		if err != nil {
+			log.Fatal(w, "Can't unmarshal: ", err.Error())
+		}
+		if transact, ok := transactions[deal.DealID]; ok {
+			card := cards[transact.Card.PAN]
+			card.blockedAmount -= deal.Amount
+			cards[card.PAN] = card
+			resp := make(map[string]string)
+			resp["Operation"] = "Success"
+			delete(transactions, deal.DealID)
+			log.Println("Charge:", cards[transact.Card.PAN])
+			_ = json.NewEncoder(w).Encode(resp)
+		} else {
+			_ = json.NewEncoder(w).Encode(UnableToExecute)
+		}
 	}
+	w.Header().Set("Content-Type", "application/json")
+
 }
 
 func handleRequests() {
@@ -230,15 +208,18 @@ func handleRequests() {
 }
 
 func main() {
-	card := Card{
-		"4012888888881881",
-		9,
-		2019,
-		100,
-		"IVANOV IVAN",
-		55555,
-		0,
+	internalCards := []Card{
+		{"4012888888881881", 9, 2019, 100, "IVANOV IVAN", 200, 0},
+		{"4539068477119696", 9, 2019, 100, "IVANOV IVAN", 200, 0},
+		{"5192679737272623", 9, 2019, 100, "IVANOV IVAN", 0, 0},
+		{"5420893164982661", 9, 2019, 100, "IVANOV IVAN", 0, 0},
+		{"344681483420255", 9, 2019, 100, "IVANOV IVAN", 1000, 0},
+		{"341487100962668", 9, 2019, 100, "IVANOV IVAN", 1000, 0},
+		{"6011755772471507", 9, 2019, 100, "IVANOV IVAN", 68000, 0},
+		{"6011937144761860", 9, 2019, 100, "IVANOV IVAN", 68315, 0},
 	}
-	cards[card.PAN] = card
+	for _, card := range internalCards {
+		cards[card.PAN] = card
+	}
 	handleRequests()
 }
